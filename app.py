584e314e0b7bf93ac3ba40cbe14d9c5a26f58215
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2023-11-08 02:02:52 +0700

    add complex numbers

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 import mandelbrot
 
+
 print(mandelbrot.sum_as_string(1, 1))
+mandelbrot.square_loop(1 + 1j)
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,18 @@
-use std::thread::spawn;
-
+use num::Complex;
 use pyo3::prelude::*;
+
+#[allow(dead_code)]
+#[pyfunction]
+fn square_add_loop(c: Complex<f64>) {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    loop {
+        z = z * z + c;
+    }
+}
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    println!("test");
-    for idx in vec![1, 2, 3, 4].iter() {
-        println!("start: {}", idx);
-        let _idx_cl = idx.clone();
-        let res = spawn(move || {
-            println!("threaded: {}", _idx_cl);
-        });
-        res.join().unwrap();
-    };
     Ok((a + b).to_string())
 }
 
@@ -21,5 +20,6 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pymodule]
 fn mandelbrot(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(square_add_loop, m)?)?;
     Ok(())
 }
